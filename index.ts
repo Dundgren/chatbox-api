11a9d3fb4970f816341a6ce88ac7b389eac99467
Dundgren <danne.l.97@hotmail.com>
2022-04-07 10:43:53 +0200

    Edited port

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import { messageRouter } from "./routes/message-router";
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 
 if (require.main === module) {
     app.listen(PORT, () => {
-        console.log(`[server]: Server is running at http://localhost:${PORT}`);
+        console.log(`[server]: Server is running at ${PORT}`);
     });
 }
 
