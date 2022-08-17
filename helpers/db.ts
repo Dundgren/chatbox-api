0da548528f97896f4bb0e14c0578887e71e98fd5
Dundgren <danne.l.97@hotmail.com>
2022-08-17 19:06:01 +0200

    Upgraded to mysql2

--- helpers/db.ts
+++ helpers/db.ts
@@ -1,25 +1,28 @@
-import mysql from 'promise-mysql';
+import mysql from 'mysql2/promise';
 
 export class Db {
-    readonly config: Record<string, unknown>;
+    readonly config: Record<string, any>;
 
     constructor() {
+        // this.config = {
+        //     host:     process.env.RDS_HOSTNAME,
+        //     port:     process.env.RDS_PORT,
+        //     user:     process.env.RDS_USERNAME,
+        //     password: process.env.RDS_PASSWORD,
+        //     database: process.env.RDS_DB_NAME,
+        //     multipleStatements: true
+        // };
         this.config = {
-            host:     process.env.RDS_HOSTNAME,
-            port:     process.env.RDS_PORT,
-            user:     process.env.RDS_USERNAME,
-            password: process.env.RDS_PASSWORD,
-            database: process.env.RDS_DB_NAME,
-            multipleStatements: true
-        };
+            host: process.env.DATABASE_URL
+        }
     }
 
     async query(sql: string, params: string[] = []) {
-        const connection = await mysql.createConnection(this.config);
-        const result = await connection.query(sql, params);
-
+        const connection =  await mysql.createConnection(this.config.host);
+        const [rows, fields]: any = await connection.execute(sql, params);
+        console.log(rows)
         connection.end();
 
-        return result;
+        return rows;
     }
 }
